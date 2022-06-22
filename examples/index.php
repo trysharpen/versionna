e1cad9a57d0ef33e9e9446b1d08b0574b163baf1
Siro Díaz Palazón <sirodiaz93@gmail.com>
2022-06-22 12:00:06 +0200

    Added composer.json and WIP index.php demo file

--- examples/index.php
+++ examples/index.php
@@ -1,16 +1,15 @@
 <?php
 
+require __DIR__ . '/vendor/autoload.php';
+
 use SiroDiaz\ManticoreMigration\Indexer\ManticoreIndexer;
 use SiroDiaz\ManticoreMigration\Manticore\ManticoreConnection;
 use SiroDiaz\ManticoreMigration\ManticoreMigration;
 use SiroDiaz\ManticoreMigration\MigrationDirector;
 use SiroDiaz\ManticoreMigration\Runner\Loader;
-use SiroDiaz\ManticoreMigration\Storage\ConnectionSingleton;
 use SiroDiaz\ManticoreMigration\Storage\DatabaseConfiguration;
 use SiroDiaz\ManticoreMigration\Storage\DatabaseConnection;
 use SiroDiaz\ManticoreMigration\Storage\MigrationTable;
-
-require __DIR__ . '/../vendor/autoload.php';
 
 //$configuration = require __DIR__ .'/config.php';
 //
@@ -54,153 +53,152 @@ require __DIR__ . '/../vendor/autoload.php';
 // php artisan manticore:populate
 // php artisan manticore:refresh
 
-function testDbConnection()
-{
-	$configuration = require __DIR__ .'/config.php';
+if (!function_exists('testDbConnection')) {
+	function testDbConnection()
+	{
+		$configuration = require __DIR__ .'/config.php';
 
-	$dbConfiguration = DatabaseConfiguration::fromArray(
-		$configuration['connection']
-	);
+		$dbConfiguration = DatabaseConfiguration::fromArray(
+			$configuration['connection']
+		);
 
-	$dbConn = new DatabaseConnection($dbConfiguration);
-	//ConnectionSingleton::setConnection($conn);
+		$dbConn = new DatabaseConnection($dbConfiguration);
 
-	// $dbConn = ConnectionSingleton::getInstance();
+		if (!$dbConn->getConnection()) {
+			exit(1);
+		}
 
-	if (!$dbConn->getConnection()) {
-		exit(1);
-	}
+		echo 'Connection is OK'. PHP_EOL;
 
-	echo 'Connection is OK'. PHP_EOL;
+		$migrationTable = new MigrationTable($dbConn, $configuration['table_prefix'], $configuration['migration_table']);
 
-	$migrationTable = new MigrationTable($dbConn, $configuration['table_prefix'], $configuration['migration_table']);
-
-	if ($migrationTable->exists()) {
-		echo 'Migration table exists'. PHP_EOL;
-	} else {
-		echo 'Migration table does not exist'. PHP_EOL;
-		$migrationTable->create();
-		echo 'Migration table created'. PHP_EOL;
-	}
-}
-
-function listMigrations()
-{
-	$configuration = require __DIR__ .'/config.php';
-
-	$dbConfiguration = DatabaseConfiguration::fromArray(
-		$configuration['connection']
-	);
-
-	$dbConn = new DatabaseConnection($dbConfiguration);
-	ConnectionSingleton::setConnection($dbConn);
-
-	$migrationTable = new MigrationTable(
-		ConnectionSingleton::getInstance(),
-		$configuration['table_prefix'],
-		$configuration['migration_table']
-	);
-
-	$migrations = $migrationTable->getSortedMigrations();
-
-	foreach ($migrations as $migration) {
-		echo $migration->getName() . PHP_EOL;
+		if ($migrationTable->exists()) {
+			echo 'Migration table exists'. PHP_EOL;
+		} else {
+			echo 'Migration table does not exist'. PHP_EOL;
+			$migrationTable->create();
+			echo 'Migration table created'. PHP_EOL;
+		}
 	}
 }
 
+if (!function_exists('listMigrations')) {
+	function listMigrations()
+	{
+		$configuration = require __DIR__ .'/config.php';
+		var_dump($configuration);
+		$dbConfiguration = DatabaseConfiguration::fromArray(
+			$configuration['connections']['pgsql'],
+		);
 
-function migrate() {
-	$configuration = require __DIR__ .'/config.php';
+		$dbConn = new DatabaseConnection($dbConfiguration);
 
-	$dbConnection = new DatabaseConnection(
-		DatabaseConfiguration::fromArray($configuration['connection'])
-	);
-
-	$manticoreConnection = new ManticoreConnection(
-		$configuration['manticore_connection']['host'],
-		$configuration['manticore_connection']['port'],
-	);
-
-	$director = new MigrationDirector();
-	$director
-		->dbConnection($dbConnection)
-		->manticoreConnection($manticoreConnection)
-		->migrationsPath($configuration['migrations_path'])
-		->migrationTable(new MigrationTable(
-			$dbConnection,
+		$migrationTable = new MigrationTable(
+			$dbConn,
 			$configuration['table_prefix'],
-			$configuration['migration_table'],
-		));
+			$configuration['migration_table']
+		);
 
-	$director->migrate();
+		$migrations = $migrationTable->getSortedMigrations();
+
+		foreach ($migrations as $migration) {
+			echo $migration->getName() . PHP_EOL;
+		}
+	}
 }
 
-function rollback($steps = 1) {
-	$configuration = require __DIR__ .'/config.php';
-
-	$dbConnection = new DatabaseConnection(
-		DatabaseConfiguration::fromArray($configuration['connection'])
-	);
-
-	$manticoreConnection = new ManticoreConnection(
-		$configuration['manticore_connection']['host'],
-		$configuration['manticore_connection']['port'],
-	);
-
-	$migrationTable = new MigrationTable($dbConnection, $configuration['table_prefix'], $configuration['migration_table']);
-
-	$director = new MigrationDirector();
-	$director
-		->dbConnection($dbConnection)
-		->manticoreConnection($manticoreConnection)
-		->migrationsPath($configuration['migrations_path'])
-		->migrationTable($migrationTable);
-
-	$director->undoMigrations($steps);
-}
-
-function refresh() {
-	$configuration = require __DIR__ .'/config.php';
-	$dbConnection = new DatabaseConnection(
-		DatabaseConfiguration::fromArray($configuration['connection'])
-	);
-	$migrationTable = new MigrationTable($dbConnection, $configuration['table_prefix'], $configuration['migration_table']);
-
-	$migrationTable->truncate();
-}
-
-function destroy() {
-	$configuration = require __DIR__ .'/config.php';
-	$dbConnection = new DatabaseConnection(
-		DatabaseConfiguration::fromArray($configuration['connection'])
-	);
-	$migrationTable = new MigrationTable($dbConnection, $configuration['table_prefix'], $configuration['migration_table']);
-
-	$migrationTable->drop();
-}
-
-function populate() {
-	$configuration = require __DIR__ .'/config.php';
-	$dbConnection = new DatabaseConnection(
-		DatabaseConfiguration::fromArray($configuration['connection'])
-	);
-
-	$manticoreConnection = new ManticoreConnection(
-		$configuration['manticore_connection']['host'],
-		$configuration['manticore_connection']['port'],
-	);
-
-	$migrationTable = new MigrationTable($dbConnection, $configuration['table_prefix'], $configuration['migration_table']);
-
-	$indexer = new ManticoreIndexer($dbConnection, $manticoreConnection);
-
-	$indexer->index('users', 'SELECT id, name, username, biography, strftime(\'%s\', birthdate) as birthdate FROM users');
-}
+//function migrate() {
+//	$configuration = require __DIR__ .'/config.php';
+//
+//	$dbConnection = new DatabaseConnection(
+//		DatabaseConfiguration::fromArray($configuration['connection'])
+//	);
+//
+//	$manticoreConnection = new ManticoreConnection(
+//		$configuration['manticore_connection']['host'],
+//		$configuration['manticore_connection']['port'],
+//	);
+//
+//	$director = new MigrationDirector();
+//	$director
+//		->dbConnection($dbConnection)
+//		->manticoreConnection($manticoreConnection)
+//		->migrationsPath($configuration['migrations_path'])
+//		->migrationTable(new MigrationTable(
+//			$dbConnection,
+//			$configuration['table_prefix'],
+//			$configuration['migration_table'],
+//		));
+//
+//	$director->migrate();
+//}
+//
+//function rollback($steps = 1) {
+//	$configuration = require __DIR__ .'/config.php';
+//
+//	$dbConnection = new DatabaseConnection(
+//		DatabaseConfiguration::fromArray($configuration['connection'])
+//	);
+//
+//	$manticoreConnection = new ManticoreConnection(
+//		$configuration['manticore_connection']['host'],
+//		$configuration['manticore_connection']['port'],
+//	);
+//
+//	$migrationTable = new MigrationTable($dbConnection, $configuration['table_prefix'], $configuration['migration_table']);
+//
+//	$director = new MigrationDirector();
+//	$director
+//		->dbConnection($dbConnection)
+//		->manticoreConnection($manticoreConnection)
+//		->migrationsPath($configuration['migrations_path'])
+//		->migrationTable($migrationTable);
+//
+//	$director->undoMigrations($steps);
+//}
+//
+//function refresh() {
+//	$configuration = require __DIR__ .'/config.php';
+//	$dbConnection = new DatabaseConnection(
+//		DatabaseConfiguration::fromArray($configuration['connection'])
+//	);
+//	$migrationTable = new MigrationTable($dbConnection, $configuration['table_prefix'], $configuration['migration_table']);
+//
+//	$migrationTable->truncate();
+//}
+//
+//function destroy() {
+//	$configuration = require __DIR__ .'/config.php';
+//	$dbConnection = new DatabaseConnection(
+//		DatabaseConfiguration::fromArray($configuration['connection'])
+//	);
+//	$migrationTable = new MigrationTable($dbConnection, $configuration['table_prefix'], $configuration['migration_table']);
+//
+//	$migrationTable->drop();
+//}
+//
+//function populate() {
+//	$configuration = require __DIR__ .'/config.php';
+//	$dbConnection = new DatabaseConnection(
+//		DatabaseConfiguration::fromArray($configuration['connection'])
+//	);
+//
+//	$manticoreConnection = new ManticoreConnection(
+//		$configuration['manticore_connection']['host'],
+//		$configuration['manticore_connection']['port'],
+//	);
+//
+//	$migrationTable = new MigrationTable($dbConnection, $configuration['table_prefix'], $configuration['migration_table']);
+//
+//	$indexer = new ManticoreIndexer($dbConnection, $manticoreConnection);
+//
+//	$indexer->index('users', 'SELECT id, name, username, biography, strftime(\'%s\', birthdate) as birthdate FROM users');
+//}
 
 // listMigrations();
 // testDbConnection();
 // populate();
 // refresh(true);
 // refresh(true);
-migrate();
+listMigrations();
 // rollback();
