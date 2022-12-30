417ac2b399bb84a72095f692f55736ba548717c3
SiroDiaz <SiroDiaz@users.noreply.github.com>
2022-12-30 13:17:43 +0000

    Fix styling

--- src/ManticoreMigration/Command/FreshCommand.php
+++ src/ManticoreMigration/Command/FreshCommand.php
@@ -14,65 +14,65 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class FreshCommand extends AbstractCommand
 {
-	protected static $defaultName = 'fresh';
+    protected static $defaultName = 'fresh';
 
-	/**
-	 * {@inheritDoc}
-	 *
-	 * @return void
-	 */
-	protected function configure(): void
-	{
-		parent::configure();
+    /**
+     * {@inheritDoc}
+     *
+     * @return void
+     */
+    protected function configure(): void
+    {
+        parent::configure();
 
-		$this->setDescription('Drop all tables and run again all migrations');
-	}
+        $this->setDescription('Drop all tables and run again all migrations');
+    }
 
-	protected function execute(InputInterface $input, OutputInterface $output): int
-	{
-		$commandExitCode = parent::execute($input, $output);
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $commandExitCode = parent::execute($input, $output);
 
-		if ($commandExitCode !== Command::SUCCESS) {
-			return $commandExitCode;
-		}
+        if ($commandExitCode !== Command::SUCCESS) {
+            return $commandExitCode;
+        }
 
-		$dbConnection = new DatabaseConnection(
-			DatabaseConfiguration::fromArray(
-				$this->configuration['connections'][$this->connection]
-			)
-		);
+        $dbConnection = new DatabaseConnection(
+            DatabaseConfiguration::fromArray(
+                $this->configuration['connections'][$this->connection]
+            )
+        );
 
-		$manticoreConnection = new ManticoreConnection(
-			$this->configuration['manticore_connection']['host'],
-			$this->configuration['manticore_connection']['port'],
-		);
+        $manticoreConnection = new ManticoreConnection(
+            $this->configuration['manticore_connection']['host'],
+            $this->configuration['manticore_connection']['port'],
+        );
 
-		$migrationTable = new MigrationTable(
-			$dbConnection,
-			$this->configuration['table_prefix'],
-			$this->configuration['migration_table'],
-		);
+        $migrationTable = new MigrationTable(
+            $dbConnection,
+            $this->configuration['table_prefix'],
+            $this->configuration['migration_table'],
+        );
 
-		$director = new MigrationDirector();
+        $director = new MigrationDirector();
 
-		$director
-			->dbConnection($dbConnection)
-			->manticoreConnection($manticoreConnection)
-			->migrationsPath($this->configuration['migrations_path'])
-			->migrationTable($migrationTable);
+        $director
+            ->dbConnection($dbConnection)
+            ->manticoreConnection($manticoreConnection)
+            ->migrationsPath($this->configuration['migrations_path'])
+            ->migrationTable($migrationTable);
 
-		if (!$migrationTable->exists()) {
-			$output->writeln('<info>Migration table doesn\'t exist</info>');
-		}
+        if (! $migrationTable->exists()) {
+            $output->writeln('<info>Migration table doesn\'t exist</info>');
+        }
 
-		try {
-			$director->fresh();
-		} catch (Exception $exception) {
-			$output->writeln($exception->getMessage());
+        try {
+            $director->fresh();
+        } catch (Exception $exception) {
+            $output->writeln($exception->getMessage());
 
-			return Command::FAILURE;
-		}
+            return Command::FAILURE;
+        }
 
-		return Command::SUCCESS;
-	}
+        return Command::SUCCESS;
+    }
 }
